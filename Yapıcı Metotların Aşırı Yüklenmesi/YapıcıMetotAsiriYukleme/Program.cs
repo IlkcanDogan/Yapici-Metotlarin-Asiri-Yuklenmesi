e3de93fa692c0f480da9aa38d1243f8a925ba84f
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-03-28 23:16:59 +0300

    Add files via upload

---
+++ Yapıcı Metotların Aşırı Yüklenmesi/YapıcıMetotAsiriYukleme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapıcıMetotAsiriYukleme
+{
+    class Program
+    {   
+        /*
+         * Yapıcı Metotların Aşırı Yüklenmesi
+         * 
+         * Tıkpı sıradan metotlar gibi yapıcı metotlarda aşırı yüklenebilir.
+         * Parametre sayısı ve parametre tipinden en az birisinin farklı olması şartıyla
+         * birden fazla yapıcı metot tanımlanabilir.
+         *-------------------------------------------------
+         *Varsayılan Yapıcı Metot
+         *
+         * Her sınıfın biz tanımlasak da tanımlamasak da bir yapıcı metodu vardır. Bu yapıcı
+         * metoda "varsayılan yapıcı metot" denilir. Default olarak sınıf içerisinde yer alan
+         * bu yapıcı metotdun görevi; sınıf içerisindeki özelliklere (değişkenlere) herhangi bir
+         * ilk değer vermediğimizde, bu özelliklerden numerik olanlara "0", bool tipinden olanlara
+         * "false" ve string gibi referans tipli olanlara da "null" değerini atamaktadır.
+         */
+        static void Main(string[] args)
+        {
+            //3.Parametre girilmediği için PI başlangıç değeri (3.1415) olacaktır.
+            Silindir s1 = new Silindir(4,6);
+            Console.WriteLine("Silindirin Hacmi: {0}\nSilindirin Yüzey Alanı: {1}\n",s1.Hacim(), s1.YuzeyAlani());
+
+            //3.Parametreye girilen PI değeri atanacaktır.
+            Silindir s2 = new Silindir(4,6,3);
+            Console.WriteLine("Silindirin Hacmi: {0}\nSilindirin Yüzey Alanı: {1}", s2.Hacim(), s2.YuzeyAlani());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Yapıcı Metotların Aşırı Yüklenmesi/YapıcıMetotAsiriYukleme/Silindir.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YapıcıMetotAsiriYukleme
+{
+    class Silindir
+    {
+        private double r, h, PI =3.1415;
+
+        //Nesne oluşturulurken double tipinden 2 parametre girildiğinde çalışacak olan yapıcı metot.
+        public Silindir(double yaricap, double yukseklik)
+        {
+            r = yaricap;
+            h = yukseklik;
+        }
+
+        //Nesne oluşturulurken double tipinden 3 parametre girildiğinde çalışacak olan yapıcı metot.
+        public Silindir(double yaricap, double yukseklik, double PiSayisi)
+        {
+            r = yaricap;
+            h = yaricap;
+            PI = PiSayisi;
+        }
+
+        public double Hacim()
+        {
+            return (PI * r * r * h);
+        }
+
+        public double YuzeyAlani()
+        {
+            return(2 * PI * r * h);
+        }
+
+
+    }
+}
